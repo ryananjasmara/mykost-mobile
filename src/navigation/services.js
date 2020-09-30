27a792496722ced6961c3da520e32a475234d26c
ryananjasmara <ryananjasmara1997@gmail.com>
2020-09-30 21:16:54 +0700

    navigation -> add services for navigations

---
+++ src/navigation/services.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {StackActions} from '@react-navigation/native';
+import {CommonActions} from '@react-navigation/native';
+
+export const navigationRef = React.createRef();
+
+function navigate(screenName, params) {
+  navigationRef.current?.dispatch(screenName, params);
+}
+
+function push(screenName, params) {
+  navigationRef.current?.dispatch(StackActions.push(screenName, params));
+}
+
+function reset(screenName) {
+  navigationRef.current?.dispatch(
+    CommonActions.reset({
+      index: 1,
+      routes: [{name: screenName}],
+    }),
+  );
+}
+
+function goBack() {
+  navigationRef.current?.goBack();
+}
+
+export default {navigate, push, reset, goBack};
