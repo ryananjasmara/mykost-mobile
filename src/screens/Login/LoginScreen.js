21ec2bdb3172ea098d4a8c1b961285514e0ef465
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-05 13:55:20 +0700

    login screen -> add template for login screen

---
+++ src/screens/Login/LoginScreen.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {WHITE} from '../../config/colors';
+
+const LoginScreen = () => {
+  /** Start Of Lifecycle Section */
+  // Did Mount
+  useEffect(() => {
+    console.log('LoginScreen.js did mount');
+  }, []);
+  /** End Of Lifecycle Section */
+  /** Start Of Functional Section */
+  /** End Of Functional Section */
+  /** Start Of Render Section */
+  return (
+    <View style={styles.mainContainer}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+  /** End Of Render Section */
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: WHITE,
+  },
+});
+
+export default LoginScreen;
