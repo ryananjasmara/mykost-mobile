7419d4cb10b36c67171bfd454606448905ccb9a7
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-05 13:54:06 +0700

    navigation -> add tab navigator, add jelajah, chat and login screen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,8 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {navigationRef} from './services';
+import {GRAY, PRIMARY} from '../config/colors';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import SplashScreen from '../screens/Splash/SplashScreen';
 import HomeScreen from '../screens/Home/HomeScreen';
+import JelajahScreen from '../screens/Jelajah/JelajahScreen';
+import ChatScreen from '../screens/Chat/ChatScreen';
+import LoginScreen from '../screens/Login/LoginScreen';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -19,48 +24,49 @@ const StackNavigator = () => {
       />
       <Stack.Screen
         name="HomeScreen"
-        component={HomeScreen}
+        component={TabNavigator}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
   );
 };
 
-// const TabNavigator = () => {
-//   return (
-//     <Tab.Navigator
-//       screenOptions={({route}) => ({
-//         tabBarIcon: ({focused}) => {
-//           let iconName;
-//           if (route.name === 'Home') {
-//             iconName = focused ? 'home' : 'home-outline';
-//           } else if (route.name === 'About') {
-//             iconName = focused
-//               ? 'information-circle'
-//               : 'information-circle-outline';
-//           } else if (route.name === 'Profile') {
-//             iconName = focused ? 'person' : 'person-outline';
-//           }
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Jelajah') {
+            iconName = focused ? 'grid' : 'grid-outline';
+          } else if (route.name === 'Chat') {
+            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+          } else if (route.name === 'Login') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
 
-//           return (
-//             <Ionicons
-//               name={iconName}
-//               size={21}
-//               color={focused ? PRIMARY : DISABLED}
-//             />
-//           );
-//         },
-//       })}
-//       tabBarOptions={{
-//         activeTintColor: PRIMARY,
-//         inactiveTintColor: DISABLED,
-//       }}>
-//       <Tab.Screen name="Home" component={HomeScreen} />
-//       <Tab.Screen name="About" component={AboutScreen} />
-//       <Tab.Screen name="Profile" component={ProfileScreen} />
-//     </Tab.Navigator>
-//   );
-// };
+          return (
+            <Ionicons
+              name={iconName}
+              size={21}
+              color={focused ? PRIMARY : GRAY}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: PRIMARY,
+        inactiveTintColor: GRAY,
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Jelajah" component={JelajahScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Login" component={LoginScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const Navigation = () => {
   const routeNameRef = React.useRef();
