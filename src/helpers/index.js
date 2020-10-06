ccceaf528a9cb2b337c60bca437996359f5a75ef
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-06 21:21:57 +0700

    helpers -> add cutLongString func, to substring if length > 130

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -11,3 +11,11 @@ export function rupiahFormat(value) {
     .replace(/\d(?=(\d{3})+\.)/g, '$&.')
     .slice(0, -3)}`;
 }
+
+export function cutLongString(string) {
+  if (string.length > 130) {
+    return `${string.substring(0, 130)}...`;
+  } else {
+    return string;
+  }
+}
