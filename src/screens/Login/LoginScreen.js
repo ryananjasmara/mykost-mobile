55b7c6420a9d2f814d75996c35eb932cdbc67f5d
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-11 03:16:02 +0700

    login screen -> finish layout

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {WHITE} from '../../config/colors';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {PRIMARY, WHITE} from '../../config/colors';
+import NavigationService from '../../navigation/services';
 
 const LoginScreen = () => {
   /** Start Of Lifecycle Section */
@@ -10,11 +11,39 @@ const LoginScreen = () => {
   }, []);
   /** End Of Lifecycle Section */
   /** Start Of Functional Section */
+  // Navigate To Login Form
+  const navigateToLoginForm = (loginType) => {
+    NavigationService.navigate('LoginFormScreen', {loginType});
+  };
   /** End Of Functional Section */
   /** Start Of Render Section */
+  // button login as pemilik
+  const renderButtonLoginAsPemilik = () => {
+    return (
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigateToLoginForm('pemilik')}>
+        <Text style={styles.buttonText}>Pemilik Properti</Text>
+      </TouchableOpacity>
+    );
+  };
+  // button login as pencari
+  const renderButtonLoginAsPencari = () => {
+    return (
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigateToLoginForm('pencari')}>
+        <Text style={styles.buttonText}>Pencari Properti</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={styles.mainContainer}>
-      <Text>Login Screen</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.black14}>Login Akun Sebagai</Text>
+      </View>
+      {renderButtonLoginAsPemilik()}
+      {renderButtonLoginAsPencari()}
     </View>
   );
   /** End Of Render Section */
@@ -26,6 +55,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: WHITE,
+  },
+  headerContainer: {
+    marginBottom: 16,
+  },
+  black14: {
+    fontSize: 30,
+  },
+  // button
+  buttonContainer: {
+    padding: 16,
+    width: '70%',
+    backgroundColor: PRIMARY,
+    borderRadius: 4,
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  buttonText: {
+    color: WHITE,
   },
 });
 
