f912d9cf7b3a4f9e8242d2a1da89e5dc3b71bc7c
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-16 15:52:29 +0700

    api services -> add fetchAPI() function

---
+++ src/services/api/fetchAPI.js
@@ -0,0 +1,42 @@
+export default async function fetchAPI({path, method, params}) {
+  const host = 'https://mykost-admin.000webhostapp.com/';
+
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const request = {
+    method,
+    headers,
+  };
+
+  if (params) {
+    request.body = JSON.stringify(params);
+  }
+
+  return fetch(`${host}${path}`, request)
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        return response.json().then((value) => {
+          return {
+            data: value.data,
+            message: value.message,
+          };
+        });
+      } else {
+        return response.json().then((value) => {
+          return {
+            message: value.message,
+          };
+        });
+      }
+    })
+    .catch((err) => {
+      return {
+        code: 503,
+        data: err,
+        message: 'Service Unavailable',
+      };
+    });
+}
