dc887b314df0657d6fd0603d6b40cfbefb24ec9e
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-16 16:17:49 +0700

    api services -> change host address

--- src/services/api/fetchAPI.js
+++ src/services/api/fetchAPI.js
@@ -1,5 +1,5 @@
 export default async function fetchAPI({path, method, params}) {
-  const host = 'https://mykost-admin.000webhostapp.com/';
+  const host = 'https://mykost-admin.000webhostapp.com/api';
 
   const headers = {
     Accept: 'application/json',
