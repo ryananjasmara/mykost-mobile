79ffd7e8ea62f2e68b5dd9d841de6e6fd625786b
ryananjasmara <ryananjasmara1997@gmail.com>
2020-10-19 07:52:38 +0700

    state -> implement persist and storage

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,21 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import {createStore, action, thunk, persist} from 'easy-peasy';
 import Methods from '../services/api/methods';
 
+const storage = {
+  async getItem(key) {
+    return JSON.parse(await AsyncStorage.getItem(key));
+  },
+  async setItem(key, data) {
+    AsyncStorage.setItem(key, JSON.stringify(data));
+  },
+  async removeItem(key) {
+    AsyncStorage.removeItem(key);
+  },
+};
+
 const userModel = {
-  user: null,
+  user: storage.getItem('user'),
   // api call / effect
   fetchUser: thunk(async (actions, payload) => {
     const response = await Methods.login(payload);
@@ -11,6 +24,11 @@ const userModel = {
   // actions
   setUser: action((state, payload) => {
     state.user = payload;
+    storage.setItem('user', payload);
+  }),
+  clearUser: action((state, payload) => {
+    state.user = null;
+    storage.setItem('user', null);
   }),
 };
 
